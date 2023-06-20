143eaafbf92ecf5d39b27b6695760f7fbe082945
Rui-Jesus <r.jesus@bmd-software.com>
2023-06-20 11:21:14 +0100

    Removed unecessary interfaces from MLProvider. Infer endpoint can now return DICOM Seg files.
    Changed type of DICOM SEG. Improved make prediction servlet
    
    Applied code review

--- sdk/src/main/java/pt/ua/dicoogle/sdk/mlprovider/MLInference.java
+++ sdk/src/main/java/pt/ua/dicoogle/sdk/mlprovider/MLInference.java
@@ -1,20 +1,30 @@
 package pt.ua.dicoogle.sdk.mlprovider;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.dcm4che2.io.DicomInputStream;
 import pt.ua.dicoogle.sdk.datastructs.dim.BulkAnnotation;
 
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 
 /**
  * This object maps predictions done by the AI algorithms.
- * It can contain a set of metrics and a list of annotations.
+ * It can contain a set of metrics, annotations and a DICOM SEG file.
  */
-public class MLPrediction {
+public class MLInference {
 
     private HashMap<String, String> metrics;
+
     private String version;
 
     private List<BulkAnnotation> annotations;
+
+    @JsonIgnore
+    private String resourcesFolder;
+
+    @JsonIgnore
+    private Path dicomSEG;
 
     public HashMap<String, String> getMetrics() {
         return metrics;
@@ -32,11 +42,31 @@ public class MLPrediction {
         this.annotations = annotations;
     }
 
+    public String getResourcesFolder() {
+        return resourcesFolder;
+    }
+
+    public void setResourcesFolder(String resourcesFolder) {
+        this.resourcesFolder = resourcesFolder;
+    }
+
+    public Path getDicomSEG() {
+        return dicomSEG;
+    }
+
+    public void setDicomSEG(Path dicomSEG) {
+        this.dicomSEG = dicomSEG;
+    }
+
     public String getVersion() {
         return version;
     }
 
     public void setVersion(String version) {
         this.version = version;
+    }
+
+    public boolean hasResults(){
+        return metrics != null || annotations != null;
     }
 }

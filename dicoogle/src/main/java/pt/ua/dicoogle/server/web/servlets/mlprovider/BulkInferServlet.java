ccc31e93b61da5040458a32b085fb92d161d6d2a
Rui-Jesus <r.jesus@bmd-software.com>
2023-07-19 14:28:57 +0100

    Code cleanup. More documentation added.

---
+++ dicoogle/src/main/java/pt/ua/dicoogle/server/web/servlets/mlprovider/BulkInferServlet.java
@@ -0,0 +1,23 @@
+package pt.ua.dicoogle.server.web.servlets.mlprovider;
+
+import org.restlet.data.Status;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BulkInferServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(BulkInferServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.sendError(Status.SERVER_ERROR_NOT_IMPLEMENTED.getCode(), "Endpoint not implemented");
+
+    }
+}
